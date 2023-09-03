ea97ecde9df8fab458629453ef4de563ac7ee95a
sagarkarki903 <sagarkarki903@gmail.com>
2023-09-03 02:20:59 -0500

    nav and upper section completed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -34,8 +34,9 @@ const Hero = () => {
           <Image src= "/hero.png" alt="hero" 
           fill className="object-contain"
           />
+          </div>
           <div className="hero__image-overlay" />
-        </div>
+        
 
       </div>
 
